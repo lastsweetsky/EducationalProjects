e1717de8a644f57174e142e657328a9cd085016d
lastsweetsky <kunonikita@gmail.com>
2022-08-11 16:04:50 +0300

    Input&print together for DragonRealm

--- python_projects/DragonRealm.py
+++ python_projects/DragonRealm.py
@@ -1,21 +1,19 @@
 import random
 import time
 
-CAVES_NUMBER = (1, 2)
+CAVES_NUMBERS = (1, 2)
 POSSIBLE_YES_ANSWERS = ('lf', 'l', 'yes', 'ye', 'y', 'да', 'д', '1', '2', '+')
 
 def displayIntro():
     print('''\nВы находитесь в землях, заселенных драконами.
 Перед собой вы видите две пещеры. В одной из них — дружелюбный дракон,
 который готов поделиться с вами своими сокровищами. Во второй —
-жадный и голодный дракон, который мигом вас съест.''')
-    print()
+жадный и голодный дракон, который мигом вас съест.\n''')
 
 def chooseCave():
     chosenCave = ''
-    while chosenCave != '1' and chosenCave != '2':
-        print('В какую пещеру вы войдете? (нажмите клавишу 1 или 2)')
-        chosenCave = input()
+    while chosenCave != str(CAVES_NUMBERS[0]) and chosenCave != str(CAVES_NUMBERS[1]):
+        chosenCave = input('В какую пещеру вы войдете? (нажмите клавишу 1 или 2)\n')
 
     return chosenCave
 
@@ -31,7 +29,7 @@ def checkCave(chosenCave):
     print_with_sleep('Ее темнота заставляет вас дрожать от страха...')
     print_with_sleep('Большой дракон выпрыгивает перед вами! Он раскрывает свою пасть и...\n')
 
-    friendlyCave = random.randint(CAVES_NUMBER[0], CAVES_NUMBER[1])
+    friendlyCave = random.randint(CAVES_NUMBERS[0], CAVES_NUMBERS[1])
 
     if chosenCave == str(friendlyCave):
         print('...делится с вами своими сокровищами!')
@@ -46,8 +44,7 @@ while playAgain in POSSIBLE_YES_ANSWERS:
     caveNumber = chooseCave()
     checkCave(caveNumber)
 
-    print(f'Попытаете удачу еще раз? (да или нет)\n' )
-    playAgain = input().lower()
+    playAgain = input('Попытаете удачу еще раз? (да или нет)\n').lower()
 else:
     print('Спасибо за игру, друг! Приходи еще.')
 
