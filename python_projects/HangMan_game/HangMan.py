59ae6bb8891b3948c7d9bea9542ab03343dbc9c6
lastsweetsky <kunonikita@gmail.com>
2022-08-19 12:44:55 +0300

    HangMan final version

--- python_projects/HangMan_game/HangMan.py
+++ python_projects/HangMan_game/HangMan.py
@@ -9,23 +9,23 @@ def getRandowWord(WORD_DICT):
     return WORD_DICT[wordKey][wordIndex], wordKey
 
 
-def displayBoard(missedLetters, correctLetters, secretWord):
-    print(HANGMAN_PICS[len(missedLetters)])
+def displayBoard(missedLetters, correctLetters, secretWord, hangman_pics):
+    print(hangman_pics[len(missedLetters)])
     print()
 
-    print('Ошибочные буквы:', end = ' ')
+    print('Ошибочные буквы:', end=' ')
     for letter in missedLetters:
-        print(letter, end = ' ')
+        print(letter, end=' ')
     print()
 
     blanks = '_' * len(secretWord)
 
     for i in range(len(secretWord)):
         if secretWord[i] in correctLetters:
-            blanks = blanks[:i] + secretWord[i] + blanks [i+1:]
+            blanks = blanks[:i] + secretWord[i] + blanks[i + 1:]
 
     for letter in blanks:
-        print(letter, end = ' ')
+        print(letter, end=' ')
     print()
 
 
@@ -45,63 +45,77 @@ def getGuess(alreadyGuessed):
 def playAgain():
     return input('Хотите сыграть еще? (да или нет)\n').lower().startswith('д')
 
+
+def smart_delete_by_indexes(list_to_operate, indexes_to_remove):
+    return [element for i, element in enumerate(list_to_operate) if i not in indexes_to_remove]
+
+
 def choose_difficulty():
+    hangman_pics = HANGMAN_PICS.copy()
+
     difficulty = 'default'
     while difficulty not in 'ЛСТ':
         difficulty = input('Выберите уровень сложности: Л - Легкий, С - Средний, Т - Тяжелый\n').upper()
     if difficulty == 'С':
-        del HANGMAN_PICS[7:]
+        hangman_pics = smart_delete_by_indexes(hangman_pics, [7, 8])
 
     if difficulty == 'Т':
-        indexes = [3, 5, 7, 8]
-        for index in sorted(indexes, reverse=True):
-            del HANGMAN_PICS[index]
+        hangman_pics = smart_delete_by_indexes(hangman_pics, [3, 5, 7, 8])
+
+    return hangman_pics
 
 
-print('В И С Е Л И Ц А')
-choose_difficulty()
-missedLetters = ''
-correctLetters = ''
-secretWord, secretSet = getRandowWord(WORD_DICT)
-gameIsDone = False
+def start_game():
 
-while True:
-    print(f'Секретное слово из набора: {secretSet}')
-    displayBoard(missedLetters, correctLetters, secretWord)
+    hangman_pics = choose_difficulty()
+    missedLetters = ''
+    correctLetters = ''
+    secretWord, secretSet = getRandowWord(WORD_DICT)
+    gameIsDone = False
 
-    guess = getGuess(missedLetters + correctLetters)
+    return hangman_pics, missedLetters, correctLetters, secretWord, secretSet, gameIsDone
 
-    if guess in secretWord:
-        correctLetters = correctLetters + guess
 
-        foundAllLetters = True
-        for i in range(len(secretWord)):
-            if secretWord[i] not in correctLetters:
-                foundAllLetters = False
+def main():
+    print('В И С Е Л И Ц А')
+
+    hangman_pics, missedLetters, correctLetters, secretWord, secretSet, gameIsDone = start_game()
+
+    while True:
+        print(f'Секретное слово из набора: {secretSet}')
+        displayBoard(missedLetters, correctLetters, secretWord, hangman_pics)
+
+        guess = getGuess(missedLetters + correctLetters)
+
+        if guess in secretWord:
+            correctLetters = correctLetters + guess
+
+            foundAllLetters = True
+            for i in range(len(secretWord)):
+                if secretWord[i] not in correctLetters:
+                    foundAllLetters = False
+                    break
+
+            if foundAllLetters:
+                print(f'ДА! Секретное слово - "{secretWord}"! Вы угадали!')
+                gameIsDone = True
+
+        else:
+            missedLetters = missedLetters + guess
+
+            if len(missedLetters) == len(hangman_pics) - 1:
+                displayBoard(missedLetters, correctLetters, secretWord, hangman_pics)
+                print(f'Вы исчерпали все попытки!\nНе угадано букв:'
+                      f' ' + str(len(missedLetters)) + ' и угадано букв: '
+                      + str(len(correctLetters)) + f'. Было загадано слово {secretWord}')
+                gameIsDone = True
+
+        if gameIsDone:
+            if playAgain():
+                hangman_pics, missedLetters, correctLetters, secretWord, secretSet, gameIsDone = start_game()
+            else:
                 break
 
-        if foundAllLetters:
-            print(f'ДА! Секретное слово - "{secretWord}"! Вы угадали!')
-            gameIsDone = True
 
-    else:
-        missedLetters = missedLetters + guess
-
-
-        if len(missedLetters) == len(HANGMAN_PICS) - 1:
-            displayBoard(missedLetters, correctLetters, secretWord)
-            print(f'Вы исчерпали все попытки!\nНе угадано букв:'
-              f' ' + str(len(missedLetters)) + ' и угадано букв: '
-              + str(len(correctLetters)) + f'. Было загадано слово {secretWord}')
-            gameIsDone = True
-
-    if gameIsDone:
-        if playAgain():
-            missedLetters = ''
-            correctLetters = ''
-            choose_difficulty()
-            gameIsDone = False
-            secretWord, secretSet = getRandowWord(WORD_DICT)
-        else:
-            break
-
+if __name__ == "__main__":
+    main()
